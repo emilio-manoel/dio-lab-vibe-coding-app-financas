807d196b276af5bf66d5cfff6b0ac2f8b8246d62
emilio <emilio.trabalho555@gmail.com>
2026-08-01 16:21:16 -0300

    adicionando o formulario ainda não funcional, sendo o input, botão de ir e voltar adicionando, sem interação alêm da indicação ao usuario qual dos passos ele está

--- website-FinanceFlow/src/components/features/Simulation/form.tsx
+++ website-FinanceFlow/src/components/features/Simulation/form.tsx
@@ -1,5 +1,16 @@
+import { PiggyBank } from "lucide-react"
+import { FormStep } from "./FormStep"
+import { StepProgress } from "./Progress"
+
 export const Simulationform = () => {
     return (
-        <div></div>
+        <>
+            <StepProgress currentStep={1} totalSteps={6}/>
+            <FormStep icon={PiggyBank} title="Renda mensal bruta" question="Quanto é deposositado na sua conta todo mês (somando todas as fontes)?" inputProps={{
+                type: 'text',
+                placeholder: 'Ex: R$ 1.800,00',
+                prefix: 'R$'
+            }}/>
+        </>
     )
 }
